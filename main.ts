9a5857a72193ec98eb004af22754e93334fd1347
Dylan Giesberts <66573865+DylanGiesberts@users.noreply.github.com>
2023-10-25 00:30:42 +0200

    Minor technical improvements; adapted plugin for pop-out windows

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-import { MarkdownView, Plugin } from 'obsidian';
+import { Plugin, TFile } from 'obsidian';
 
 export default class TableCheckboxesPlugin extends Plugin {
 	async onload() {
 
-		this.registerDomEvent(document, 'keyup', (evt: KeyboardEvent): void => {
-			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-			if (evt.key == "]" && view) {
+		this.registerDomEvent(activeWindow, "keyup", (evt: KeyboardEvent): void => {
+			if (evt.key == "]") {
+				const view = this.app.workspace.activeEditor;
+				if (!view || !view.editor) {
+					return;
+				}
 				const location = view.editor.getCursor("anchor");
 				const rowValue = view.editor.getLine(location.line);
 				if (this.isMDCheckboxInTable(rowValue)) {
@@ -13,36 +16,38 @@ export default class TableCheckboxesPlugin extends Plugin {
 					const start = {...location}; // Shallow copy
 					start.ch -= checkBox.length; // Subtract the length from the location of ']'
 					view.editor.setSelection(start, location); // Select '-[]'
-					const checkboxId = this.generateUniqueCheckboxId(view);
+					const checkboxId = this.generateUniqueCheckboxId(view.editor.getDoc().getValue());
 					view.editor.replaceSelection(`<input type="checkbox" unchecked id="${checkboxId}">`); // Replace selection with unchecked HTML checkbox
 				}
 			}
 		});
 
-		this.registerDomEvent(document, "change", (evt: InputEvent): void => {
-			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		this.registerDomEvent(activeWindow, "change", (evt: InputEvent): void => {
 			// Check for data-task attribute to ignore markdown checkboxes
-			if (evt.target instanceof HTMLInputElement && view && evt.target.id && evt.target.hasAttribute("data-task") === false) {
-				const checkbox = evt.target;
-				if (checkbox.getAttribute("type") === "checkbox") {
-					const page = view.getViewData();
-					const id = evt.target.id;
-					this.toggleCheckbox(page, view, checkbox.checked, id);
+			const changeEl = evt.target as Element;
+			if (changeEl.instanceOf(HTMLInputElement) && changeEl.id && changeEl.hasAttribute("data-task") === false) {
+				const view = this.app.workspace.activeEditor;
+				if (!view || !view.editor || !view.file) {
+					return;
+				}
+				if (changeEl.getAttribute("type") === "checkbox") {
+					const page = view.editor.getDoc().getValue();
+					const id = changeEl.id;
+					this.toggleCheckbox(page, view.file, changeEl.checked, id);
 				}
 			}
 		})
 	}
 
-	private generateUniqueCheckboxId(view: MarkdownView): string {
+	private generateUniqueCheckboxId(page: string): string {
 		let id = crypto.randomUUID().slice(-6);
-		while (this.idExistsInFile(id, view)) {
+		while (this.idExistsInFile(id, page)) {
 			id = crypto.randomUUID();
 		}
 		return id;
 	}
 
-	private idExistsInFile(id: string, view: MarkdownView): boolean {
-		const page = view.getViewData();
+	private idExistsInFile(id: string, page: string): boolean {
 		const idIndex = page.search(id);
 		return idIndex !== -1;
 	}
@@ -63,8 +68,8 @@ export default class TableCheckboxesPlugin extends Plugin {
 		return checkboxMatch![0];
 	}
 
-	private toggleCheckbox(page: string, view: MarkdownView, isChecked: boolean, checkboxId: string): void {
+	private toggleCheckbox(page: string, file: TFile, isChecked: boolean, checkboxId: string): void {
 		page = page.replace(new RegExp(`<input type="checkbox" (un)?checked id="${checkboxId}">`), `<input type="checkbox" ${isChecked ? "" : "un"}checked id="${checkboxId}">`);
-		this.app.vault.modify(view.file, page);
+		this.app.vault.modify(file, page);
 	}
 }
